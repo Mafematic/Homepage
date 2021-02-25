c5386ff44573fd83e9fd5c51360eb8d93ca84b3c
Mafematic <thisismafematic@gmail.com>
2021-02-25 17:31:53 +0100

    Added functionality and content to portfolio page

--- python.py
+++ python.py
@@ -2,14 +2,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('./index.html')
 
-@app.route('/blog')
-def blog(): 
-	return 'These are my thoughts on blogs'
+@app.route('/')
+def my_home(): 
+	return render_template('index.html')
+
+@app.route('/index')
+def index(): 
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/components')
+def components(): 
+	return render_template('components.html')
+
+@app.route('/contact')
+def contact(): 
+	return render_template('contact.html')
+
+@app.route('/work')
+def work(): 
+	return render_template('work.html')
+
+@app.route('/works')
+def works(): 
+	return render_template('works.html')
 
 @app.route('/blog/2020/dogs')
 def blog2():
 	return 'this is my dog'
+
