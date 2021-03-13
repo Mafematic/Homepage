0287309429a1c2c0feb6631250d80b41601c8e6a
Mafematic <thisismafematic@gmail.com>
2021-03-13 16:26:50 +0100

    Added more functionality and updated the form

--- python.py
+++ python.py
@@ -1,37 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
-print(__name__)
 
+print(__name__)
 
 @app.route('/')
 def my_home(): 
 	return render_template('index.html')
 
-@app.route('/index')
-def index(): 
-	return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
-@app.route('/components')
-def components(): 
-	return render_template('components.html')
-
-@app.route('/contact')
-def contact(): 
-	return render_template('contact.html')
-
-@app.route('/work')
-def work(): 
-	return render_template('work.html')
-
-@app.route('/works')
-def works(): 
-	return render_template('works.html')
-
-@app.route('/blog/2020/dogs')
-def blog2():
-	return 'this is my dog'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST': 
+    	data = request.form.to_dict()
+    	print(data)
+    	return redirect('/thankyou.html')
+    else: 
+    	return 'Something went wrong. Try again.'
 
